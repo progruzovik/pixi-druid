45500a466763e4b3793fb786a3796cc4b02e3414
Alexandr Ilyushechkin <progruzovik@gmail.com>
2020-07-19 13:27:25 +0300

    improve ScalableBranch

--- src/ts/ui/ScalableBranch.ts
+++ src/ts/ui/ScalableBranch.ts
@@ -1,14 +1,13 @@
-import { Branch, Event } from "../";
+import { Branch } from "../";
 import * as PIXI from "pixi.js";
 
+// noinspection JSUnusedGlobalSymbols
 export class ScalableBranch extends Branch {
 
-    constructor() {
-        super();
-        this.on(Event.RESIZE, (width: number, height: number) => {
-            const bounds: PIXI.Rectangle = this.getLocalBounds();
-            const ratio: number = Math.min(width / bounds.width, height / bounds.height);
-            this.scale.set(ratio, ratio);
-        });
+    resize(width: number, height: number) {
+        const bounds: PIXI.Rectangle = this.getLocalBounds();
+        const ratio: number = Math.min(width / bounds.width, height / bounds.height);
+        this.scale.set(ratio, ratio);
+        super.resize(width, height);
     }
 }
